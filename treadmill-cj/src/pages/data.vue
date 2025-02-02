f10dc0b2659854cba8ed6ba353cbca8d00273a0a
Lucas Speers <lucas_speers@outlook.com>
2025-02-02 08:19:38 -0700

    video

--- treadmill-cj/src/pages/data.vue
+++ treadmill-cj/src/pages/data.vue
@@ -9,6 +9,12 @@
         <v-row><Graph /></v-row>
         <v-row><WebSocket /></v-row>
       </v-col>
+      <v-col>
+        <video controls>
+          <source src="/src/assets/BigBuckBunny_320x180.mp4" type="video/mp4">
+          does not work
+        </video>
+      </v-col>
     </v-row>
   </v-container>
   <!-- <RawData />
